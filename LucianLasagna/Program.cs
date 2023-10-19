0721132486ec20a4ed1b427906dd2153bdf5533e
Santiago De Blasis <deblasissantiago@gmail.com>
2023-10-18 23:04:08 -0300

    Add Lucian's Lasagna solution

--- LucianLasagna/Program.cs
+++ LucianLasagna/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+class Lasagna
+{
+    private const int MinutesToCook = 40;
+    private const int MinutesPerLayer = 2;
+
+    public int ExpectedMinutesInOven() => MinutesToCook;
+
+    public int RemainingMinutesInOven(int minutesInTheOven) => MinutesToCook - minutesInTheOven;
+
+    public int PreparationTimeInMinutes(int numberOfLayers) => numberOfLayers * MinutesPerLayer;
+
+    public int ElapsedTimeInMinutes(int numberOfLayers, int minutesInTheOven) =>
+        PreparationTimeInMinutes(numberOfLayers) + minutesInTheOven;
+}
