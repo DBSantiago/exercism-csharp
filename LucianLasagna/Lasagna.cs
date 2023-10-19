8f050bbbde36c2346f2c6a2ab0c34d9e6f203b11
Santiago De Blasis <deblasissantiago@gmail.com>
2023-10-18 23:12:24 -0300

    Refactor Lucian's Lasagna

---
+++ LucianLasagna/Lasagna.cs
@@ -0,0 +1,18 @@
+﻿namespace LucianLasagna;
+
+class Lasagna
+{
+    private const int MinutesToCook = 40;
+    private const int MinutesPerLayer = 2;
+
+    public static int ExpectedMinutesInOven() => MinutesToCook;
+
+    public static int RemainingMinutesInOven(int minutesInTheOven) =>
+        MinutesToCook - minutesInTheOven;
+
+    public static int PreparationTimeInMinutes(int numberOfLayers) =>
+        numberOfLayers * MinutesPerLayer;
+
+    public static int ElapsedTimeInMinutes(int numberOfLayers, int minutesInTheOven) =>
+        PreparationTimeInMinutes(numberOfLayers) + minutesInTheOven;
+}
--- LucianLasagna/Program.cs
+++ LucianLasagna/Program.cs
@@ -1,16 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-class Lasagna
-{
-    private const int MinutesToCook = 40;
-    private const int MinutesPerLayer = 2;
-
-    public int ExpectedMinutesInOven() => MinutesToCook;
-
-    public int RemainingMinutesInOven(int minutesInTheOven) => MinutesToCook - minutesInTheOven;
-
-    public int PreparationTimeInMinutes(int numberOfLayers) => numberOfLayers * MinutesPerLayer;
-
-    public int ElapsedTimeInMinutes(int numberOfLayers, int minutesInTheOven) =>
-        PreparationTimeInMinutes(numberOfLayers) + minutesInTheOven;
-}
+Console.WriteLine("Exercism - C# Track - Lucian's Lasagna");
