2839ef325df5290e4bb735a164de763a7436af96
Santiago De Blasis <deblasissantiago@gmail.com>
2023-10-18 23:42:53 -0300

    Add Annalyn's Infiltration problem solution

---
+++ AnnalynsInfiltration/QuestLogic.cs
@@ -0,0 +1,17 @@
+﻿namespace AnnalynsInfiltration;
+
+static class QuestLogic
+{
+    public static bool CanFastAttack(bool knightIsAwake) => !knightIsAwake;
+
+    public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake) =>
+        knightIsAwake || archerIsAwake || prisonerIsAwake;
+
+    public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake) =>
+        prisonerIsAwake && !archerIsAwake;
+
+    public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake,
+        bool petDogIsPresent) => (petDogIsPresent && !archerIsAwake) ||
+                                 (!petDogIsPresent && prisonerIsAwake && !archerIsAwake &&
+                                  !knightIsAwake);
+}
