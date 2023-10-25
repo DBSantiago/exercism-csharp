d17e84da1a1f63f21ddf7a8f2e9037680b25bcdf
Santiago De Blasis <deblasissantiago@gmail.com>
2023-10-25 00:18:12 -0300

    Add LogLevels problem solution

---
+++ LogLevels/LogAnalysis.cs
@@ -0,0 +1,15 @@
+﻿namespace LogLevels;
+
+public static class LogAnalysis
+{
+    public static string SubstringAfter(this string str, string stringDelimiter) =>
+        str.Split(stringDelimiter)[1];
+
+    public static string
+        SubstringBetween(this string str, string stringStart, string stringEnd) =>
+        str.SubstringAfter(stringStart).Split(stringEnd)[0];
+
+    public static string Message(this string str) => str.SubstringAfter(": ");
+
+    public static string LogLevel(this string str) => str.SubstringBetween("[", "]");
+}
